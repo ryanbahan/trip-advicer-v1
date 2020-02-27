0a9d249e0abc189b0603888c5855e45aafb03028
Ryan Bahan <ryan@ryanbahan.com>
2020-02-26 18:25:25 -0700

    Begin building login authenticator

--- src/classes/authenticator.js
+++ src/classes/authenticator.js
@@ -1,10 +1,18 @@
 class Authenticator {
   constructor() {
-
+    this.password = 'travel2020';
   }
 
   static validate(username, password) {
-    return `${username} ${password}`
+    this.validateUsername(username);
+    return this.validateUsername(username);
+  }
+
+  static validateUsername(username) {
+    let regex = /traveler[0-50]/;
+    let match = name.match(regex);
+
+    return regex.test(username);
   }
 }
 export default Authenticator;
