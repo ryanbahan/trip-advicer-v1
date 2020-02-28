580b8fff241aee2fc6e6675724de2933c52ef83f
Ryan Bahan <ryan@ryanbahan.com>
2020-02-27 17:18:46 -0700

    Initial structure for traveler dashboard

--- src/classes/dashboard.js
+++ src/classes/dashboard.js
@@ -8,7 +8,35 @@ class Dashboard {
 
   displayTravelerDashboard(user) {
     this.currentUser = new Traveler(user);
-    return `<div>traveler!</div>`
+    return `<nav><h1>TripAdvicer</h1></nav><div class="user-options">
+    <div class=options-top><h2>Your Trips</h2><p class="total-spent">Total Expenses: $XX</p></div>
+    <div class="options-buttons"><button>Pending</button><button>Approved</button><button>Past</button></div>
+    </div>
+    <main id="grid-content">
+    <section class="book-trip-card">
+    <h3>Book a new trip!</h3>
+    </section>
+    <section class="trip-card">
+    <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80">
+    <h3>Trip name</h3>
+    </section>
+    <section class="trip-card">
+    <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80">
+    <h3>Trip name</h3>
+    </section>
+    <section class="trip-card">
+    <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80">
+    <h3>Trip name</h3>
+    </section>
+    <section class="trip-card">
+    <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80">
+    <h3>Trip name</h3>
+    </section>
+    <section class="trip-card">
+    <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80">
+    <h3>Trip name</h3>
+    </section>
+    </main>`
   }
 
   displayAdminDashboard() {
